dadb5a302cb13d7685e204f7f60897d6301c5ea0
Lukas1157 <lukasuhlmann999@gmail.com>
2024-12-12 11:46:38 +0100

    Leertasten-funktion rausgenommen

--- Assets/Scripts/Tower/HealthTower.cs
+++ Assets/Scripts/Tower/HealthTower.cs
@@ -16,17 +16,7 @@ public class HealthTower : MonoBehaviour
         healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(1);
-        }
-    }
-
-
-
+ 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
